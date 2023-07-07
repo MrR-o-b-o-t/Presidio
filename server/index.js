0a532b125156133e9af13e1ee147f6872083f4ed
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-07-07 09:41:23 -0500

    Update index.js in server folder to serve frontend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 const searchController = require("./controllers/searchController");
 
 const app = express();
 app.use(cors());
+const publicPath = path.join(__dirname, "../client/build");
+app.use(express.static(publicPath));
+
 const port = 5000;
 
 app.get("/api/search/:query", searchController.search);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
