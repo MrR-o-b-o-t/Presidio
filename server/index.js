831f60dd62bcd96b2bb6e124e35332228e2ddc7d
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-07-08 14:53:13 -0500

    Update port in server/index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const publicPath = path.join(__dirname, "../client/build");
 app.use(express.static(publicPath));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.get("/api/search/:query", searchController.search);
 
